1626deeacdef0c513fac7be8dfd49daa706b0ba1
it21059940 <87602629+it21059940@users.noreply.github.com>
2021-07-25 14:07:17 +0000

    exercise 4 completed

--- Tute04.c
+++ Tute04.c
@@ -6,6 +6,10 @@ Do not change the code given in the main() function when you are implementing yo
 
 #include <stdio.h>
 
+int maximum(int no1,int no2);
+int minimum(int no1,int no2);
+int multiply(int no1,int no2);
+
 int main() {
    int no1, no2;
    printf("Enter a value for no 1 : ");
@@ -17,3 +21,30 @@ int main() {
    printf("%d ", multiply(no1, no2));
    return 0;
 }
+int maximum(int no1,int no2)
+{
+	if(no1>no2)
+	{
+		return no1;
+	}
+	else
+	{
+		return no2;
+	}
+}
+int minimum(int no1,int no2)
+{
+	if(no1<no2)
+	{
+		return no1;
+	}
+	else
+	{
+		return no2;
+	}
+}
+int multiply(int no1,int no2)
+{
+	return no1*no2;
+}
+
