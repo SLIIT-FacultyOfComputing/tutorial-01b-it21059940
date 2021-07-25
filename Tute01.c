db91e7d8d788c195a5340e0250903b4dc6674c2f
it21059940 <87602629+it21059940@users.noreply.github.com>
2021-07-25 14:01:50 +0000

    exercise 1 completed

--- Tute01.c
+++ Tute01.c
@@ -4,8 +4,22 @@
 
 #include <stdio.h>
 
-int main() {
-  
-  return 0;
+int main() 
+{
+  int mark1,mark2;
+	float total,avg;
+	
+	printf("Enter mark 1:");
+	scanf("%d",&mark1);
+	
+	printf("Enter mark 2:");
+	scanf("%d",&mark2);
+	
+	total=mark1+mark2;
+	avg=total/2;
+	
+	printf("Average:%.2f",avg);
+	
+	return 0;
 }
 
